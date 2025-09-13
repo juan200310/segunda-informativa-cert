4f89ad840582e3ce18ee69022166abe9873f003b
Juan Manuel López <juanmanuel.lopez@ingco.co>
2025-09-13 12:57:27 -0500

    se corrige menu hamburguesa en barra de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,16 +11,24 @@ export class AppComponent implements OnInit{
   title = 'pagina-informativa-mega';
   isMenuOpen = false;
   active:string='inicio'
+
   constructor(private route: ActivatedRoute,private router: Router){
-   
+
   }
   ngOnInit(): void {
-   
+
+  }
+  toggleMenu(activeLink?: string) {
+    if (activeLink) {
+      this.active = activeLink;
+      this.isMenuOpen = false;
+    } else {
+      this.isMenuOpen = !this.isMenuOpen;
+    }
   }
 
-  toggleMenu(tab:string) {
-    this.isMenuOpen = !this.isMenuOpen;
-    this.active=tab
+  closeMenu() {
+    this.isMenuOpen = false;
   }
   toggleMenuActive(tab:string){
     if(this.isMenuOpen){
